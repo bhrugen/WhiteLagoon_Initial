d195191304f369301a9c049a5220053b545f86e7
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-02 10:24:51 -0500

    Get Villas

---
+++ WhiteLagoon/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Data;
+using WhiteLagoon.Models;
+
+namespace WhiteLagoon.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public VillaController(ApplicationDbContext context)
+        {
+                _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Villa> villaList = _context.Villas;
+            return View(villaList);
+        }
+    }
+}

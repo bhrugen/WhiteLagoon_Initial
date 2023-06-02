3ad3b66eaacb6cc42a4c92add2b6b5d97b566aa8
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-02 11:08:58 -0500

    Multiple ways to retrieve records

--- WhiteLagoon/Controllers/VillaController.cs
+++ WhiteLagoon/Controllers/VillaController.cs
@@ -40,6 +40,21 @@ namespace WhiteLagoon.Controllers
         public IActionResult Update(int villaId)
         {
             Villa obj = _context.Villas.FirstOrDefault(x => x.Id == villaId);
+
+            //will retrieve first record if more than one records are found. If no records are found it will return null
+            //Villa obj1 = _context.Villas.Where(x => x.Id == villaId).FirstOrDefault();
+
+            // will throw exception if no records are found
+            //https://localhost:7049/Villa/Update?villaId=7
+            //Villa obj2 = _context.Villas.First(x => x.Id == villaId);
+            
+            // will throw exception if more than one records are found
+            //Villa obj3 = _context.Villas.SingleOrDefault(x => x.Name.ToLower().Contains("villa11"));
+            
+            // will throw exception if more than one records are found or if no records are found
+            //Villa obj4 = _context.Villas.Single(x => x.Name.ToLower().Contains("villa11"));
+
+
             return View(obj);
         }
        

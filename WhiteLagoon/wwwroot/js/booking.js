9fedb67adad203012ff264a368eb3b5a907339e8
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-08 16:11:51 -0500

    filters functional

---
+++ WhiteLagoon/wwwroot/js/booking.js
@@ -0,0 +1,56 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    const urlParams = new URLSearchParams(window.location.search);
+    const status = urlParams.get('status');
+    console.log(status);
+    loadDataTable(status);
+    
+});
+
+function loadDataTable(status) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/booking/getall?status=' + status },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phone', "width": "10%" },
+            { data: 'email', "width": "15%" },
+            { data: 'status', "width": "15%" },
+            { data: 'totalCost', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/booking/details?bookingId=${data}" class="btn btn-outline-warning mx-2"> <i class="bi bi-pencil-square"></i> Details</a>               
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    })
+}

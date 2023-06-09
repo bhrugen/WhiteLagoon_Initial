49f6dc3bc69252331e04b5e0f09d4256ed8b9a12
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-09 09:32:53 -0500

    Seed Villa Numbers

--- WhiteLagoon_DataAccess/Data/ApplicationDbContext.cs
+++ WhiteLagoon_DataAccess/Data/ApplicationDbContext.cs
@@ -115,6 +115,58 @@ namespace WhiteLagoon_DataAccess
                    VillaId = 3,
                    Name = "Private Balcony"
                });
+
+            modelBuilder.Entity<VillaNumber>().HasData(
+              new VillaNumber
+              {
+                  Villa_Number = 101,
+                  VillaId = 1
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 102,
+                  VillaId = 1
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 103,
+                  VillaId = 1
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 104,
+                  VillaId = 2
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 105,
+                  VillaId = 2
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 106,
+                  VillaId = 2
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 107,
+                  VillaId = 3
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 108,
+                  VillaId = 3
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 109,
+                  VillaId = 3
+              }, 
+              new VillaNumber
+              {
+                  Villa_Number = 110,
+                  VillaId = 3
+              });
         }
     }
 }
---
+++ WhiteLagoon_DataAccess/Migrations/20230609143140_trte3.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class trte3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "VillaNumbers",
+                columns: new[] { "Villa_Number", "SpecialDetails", "VillaId" },
+                values: new object[,]
+                {
+                    { 101, null, 1 },
+                    { 102, null, 1 },
+                    { 103, null, 1 },
+                    { 104, null, 2 },
+                    { 105, null, 2 },
+                    { 106, null, 2 },
+                    { 107, null, 3 },
+                    { 108, null, 3 },
+                    { 109, null, 3 },
+                    { 110, null, 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 101);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 102);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 103);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 104);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 105);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 106);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 107);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 108);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 109);
+
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 110);
+        }
+    }
+}

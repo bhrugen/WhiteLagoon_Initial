af16e89d3301a66a14f123f0821afb50474bbc9f
Ranjan Sharma <ranjansharma.logic2software@outlook.com>
2023-06-11 12:15:30 +0530

    Milestone 2: "Avaibility of Villa"

--- WhiteLagoon/Controllers/HomeController.cs
+++ WhiteLagoon/Controllers/HomeController.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
 using System.Diagnostics;
+using System.Linq;
+using WhiteLagoon_DataAccess.Repository;
 using WhiteLagoon_DataAccess.Repository.IRepository;
 using WhiteLagoon_Models;
 using WhiteLagoon_Models.ViewModels;
+using WhiteLagoon_Utility;
 
 namespace WhiteLagoon.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
+        private readonly List<string> _bookedStatus = new List<string> { "Approved", "CheckedIn" };
         public HomeController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -27,15 +33,23 @@ namespace WhiteLagoon.Controllers
         public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
         {
             var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
-            foreach(var villa in villaList)
-            {
-                //based on date get availability
-                if (villa.Id % 2 == 0)
-                {
-                    villa.IsAvailable = true;
-                }
 
+            var bookedVillas = _unitOfWork.Booking.GetAll().Where(m => m.CheckInDate == checkInDate).ToList();
+
+            if (bookedVillas.Count()  > 0)
+            {
+                foreach (var villa in villaList)
+                {
+                    foreach (var item in bookedVillas)
+                    {
+                        if (_bookedStatus.Contains(item.Status) && villa.Id == item.VillaId)
+                        {
+                            villa.IsAvailable = false;
+                        }
+                    }
+                }
             }
+
             HomeVM homeVM = new ()
             {
                 CheckInDate = checkInDate,
@@ -44,9 +58,6 @@ namespace WhiteLagoon.Controllers
             };
             return PartialView("_VillaList", homeVM);
         }
-
-
-
 
         public IActionResult Details(int villaId)
         {

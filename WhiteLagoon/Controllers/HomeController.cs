b6a390a492e58ee89d50c127ba7849233a00814e
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-15 14:48:04 -0500

    Dynamic Room Availability

--- WhiteLagoon/Controllers/HomeController.cs
+++ WhiteLagoon/Controllers/HomeController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
 using System.Diagnostics;
+using System.Linq;
+using WhiteLagoon_DataAccess.Repository;
 using WhiteLagoon_DataAccess.Repository.IRepository;
 using WhiteLagoon_Models;
 using WhiteLagoon_Models.ViewModels;
+using WhiteLagoon_Utility;
+using System.Linq;
+using NuGet.Versioning;
 
 namespace WhiteLagoon.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
+        private readonly List<string> _bookedStatus = new List<string> { "Approved", "CheckedIn" };
         public HomeController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -18,7 +26,7 @@ namespace WhiteLagoon.Controllers
             HomeVM homeVM = new HomeVM()
             {
                 VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList(),
-                Nights=1
+                Nights = 1
             };
             return View(homeVM);
         }
@@ -27,16 +35,33 @@ namespace WhiteLagoon.Controllers
         public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
         {
             var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
-            foreach(var villa in villaList)
-            {
-                //based on date get availability
-                if (villa.Id % 2 == 0)
-                {
-                    villa.IsAvailable = true;
-                }
 
+            var VillaNumumbers = _unitOfWork.VillaNumber.GetAll().ToList();
+
+            var bookedVillas = _unitOfWork.Booking.GetAll().ToList();
+
+            foreach (var villa in villaList)
+            {
+                for (int i = 0; i < nights; i++)
+                {
+                    var isToBeCheckout = bookedVillas.Where(m => m.VillaId == villa.Id && m.CheckOutDate == checkInDate.AddDays(i)).ToList();
+
+                    var totBookedVillas = bookedVillas.Where(m => m.CheckInDate <= checkInDate.AddDays(i) && m.CheckOutDate >= checkInDate.AddDays(i) &&
+                                          m.VillaId == villa.Id &&
+                                          _bookedStatus.Any(i => i.ToString() == m.Status)).ToList();
+
+                    var totRoomsInVilla = VillaNumumbers.Where(m => m.VillaId == villa.Id).ToList();
+
+                    var totAvailRooms = totRoomsInVilla.Count() - totBookedVillas.Count();
+
+                    if (totAvailRooms == 0 && isToBeCheckout.Count() == 0)
+                    {
+                        villa.IsAvailable = false;
+                    }
+                }
             }
-            HomeVM homeVM = new ()
+
+            HomeVM homeVM = new()
             {
                 CheckInDate = checkInDate,
                 VillaList = villaList,
@@ -45,14 +70,11 @@ namespace WhiteLagoon.Controllers
             return PartialView("_VillaList", homeVM);
         }
 
-
-
-
         public IActionResult Details(int villaId)
         {
-            DetailsVM detailsVM = new ()
+            DetailsVM detailsVM = new()
             {
-                Villa= _unitOfWork.Villa.Get(u=>u.Id==villaId),
+                Villa = _unitOfWork.Villa.Get(u => u.Id == villaId),
                 Nights = 1
             };
             return View(detailsVM);

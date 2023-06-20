b479c18ec3cb6c4b6c03d49b693abc4196bbb9b0
Ravi <ravidudhatra@gmail.com>
2023-06-21 00:49:38 +0530

    Added Summernotes in vIlla create and update page and for checkin date prevent past date selection in home page.

---
+++ WhiteLagoon/wwwroot/js/createVilla.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('#frmVilla').validate({
+        ignore: ":hidden:not(#rtDescription),.note-editable.panel-body"
+    });
+
+    $('#rtDescription').summernote({
+        height: 200,
+    });
+
+    $('form').each(function () {
+        if ($(this).data('validator'))
+            $(this).data('validator').settings.ignore = ".note-editor *";
+    });
+});
--- WhiteLagoon/wwwroot/js/villa.js
+++ WhiteLagoon/wwwroot/js/villa.js
@@ -48,3 +48,4 @@ function Delete(url) {
         }
     })
 }
+
